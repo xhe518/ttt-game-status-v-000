c903b1168a776c75000ddb527e8a779cab344dcb
Sid He <xhe518@gmail.com>
2018-04-25 02:29:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,9 @@ def won?(board)
     position_2 = board[win_combination[1]]
     position_3 = board[win_combination[2]]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      win_combination
     else
       false
     end
